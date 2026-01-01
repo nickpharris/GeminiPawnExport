1c12226c1a4763b3b760d4daef36f26665b5d7d0
nickpharris <nickpharris@gmail.com>
2026-01-01 00:32:13 -0800

    Cleaned-up implementation ModSettings (GeminiSettings)

--- GeminiSettings.cs
+++ GeminiSettings.cs
@@ -4,14 +4,25 @@ namespace GeminiPawnExport
 {
     public class GeminiSettings : ModSettings
     {
+        // 1. Define the default value once as a constant.
+        private const string DefaultPromptText =
+            "In Rimworld, how should I optimally allocate all of my weapons to all of my pawns. " +
+            "Please take into account which injuries (if any) are temporary vs permanent, " +
+            "and allocate based on when they have recovered their fitness from any temporary injuries. " +
+            "Make any recommendations about how I should reallocate clothing / armor (inc. recon helmets) " +
+            "based on weapons strategy - considering their roles once they have recovered from any temporary battle injuries.";
+
         public string apiKey = "";
-        // Updated prompt to explicitly request a table format
-        public string defaultPrompt = "Analyze the following RimWorld colonist data. Identify inefficiencies in weapon and armor allocation based on pawn skills (Shooting/Melee) and traits. Suggest specific reallocations to optimize defense. Please output the results as a Markdown table.";
+
+        // 2. Initialize the field using the constant.
+        public string defaultPrompt = DefaultPromptText;
 
         public override void ExposeData()
         {
+            // The third argument is the default value used if the setting is missing in the XML file.
             Scribe_Values.Look(ref apiKey, "apiKey", "");
-            Scribe_Values.Look(ref defaultPrompt, "defaultPrompt", "Analyze the following RimWorld colonist data. Identify inefficiencies in weapon and armor allocation based on pawn skills (Shooting/Melee) and traits. Suggest specific reallocations to optimize defense. Please output the results as a Markdown table.");
+            Scribe_Values.Look(ref defaultPrompt, "defaultPrompt", DefaultPromptText);
+
             base.ExposeData();
         }
     }
